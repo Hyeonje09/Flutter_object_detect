3ee30687954835aa00b45f0520f373e7bc07cb75
Hyeonje09 <jhj37299455@gmail.com>
2023-08-21 18:10:56 +0900

    분리 배출 텍스트 창 추가

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -6,7 +6,6 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_pytorch/pigeon.dart';
 import 'package:flutter_pytorch/flutter_pytorch.dart';
-import 'package:object_detection/LoaderState.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,7 +16,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late ModelObjectDetection _objectModel;
-  String? _imagePrediction;
   List<String> _prediction = [];
   File? _image;
   ImagePicker _picker = ImagePicker();
@@ -26,6 +24,11 @@ class _HomeScreenState extends State<HomeScreen> {
   bool firststate = false;
   bool message = true;
   bool _popupOpen = false;
+  bool _showImageDetails = false;
+  final Map<String, String> labelTranslations = {
+    "clock": "시계",
+    // 더 많은 라벨에 대한 번역 추가
+  };
 
   @override
   void initState() {
@@ -71,19 +74,29 @@ class _HomeScreenState extends State<HomeScreen> {
         minimumScore: 0.1,
         IOUThershold: 0.3);
     objDetect.forEach((element) {
-      print({
-        "score": element?.score,
-        "className": element?.className,
-        "class": element?.classIndex,
-        "rect": {
-          "left": element?.rect.left,
-          "top": element?.rect.top,
-          "width": element?.rect.width,
-          "height": element?.rect.height,
-          "right": element?.rect.right,
-          "bottom": element?.rect.bottom,
-        },
-      });
+      if (element != null) {
+        String englishLabel = element.className ?? "";
+        String translatedLabel = labelTranslations[englishLabel] ?? englishLabel;
+        print({
+          "score": element?.score,
+          "className": element?.className,
+          "class": element?.classIndex,
+          "rect": {
+            "left": element?.rect.left,
+            "top": element?.rect.top,
+            "width": element?.rect.width,
+            "height": element?.rect.height,
+            "right": element?.rect.right,
+            "bottom": element?.rect.bottom,
+          },
+        });
+        if (translatedLabel.isNotEmpty) {
+          String prediction = "$translatedLabel (${element.score.toStringAsFixed(2)})";
+          setState(() {
+            _prediction.add(prediction);
+          });
+        }
+      }
     });
     scheduleTimeout(5 * 1000);
     setState(() {
@@ -95,6 +108,19 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _popupOpen = true;
     });
+
+    // Find the prediction with the highest probability
+    String highestProbabilityPrediction = "";
+    double highestProbability = 0.0;
+    for (String prediction in _prediction) {
+      double probability = double.parse(
+          prediction.substring(prediction.lastIndexOf("(") + 1, prediction.lastIndexOf(")")));
+      if (probability > highestProbability) {
+        highestProbability = probability;
+        highestProbabilityPrediction = prediction;
+      }
+    }
+
 
     await showDialog(
       context: context,
@@ -115,22 +141,17 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                         : Container(),
                     SizedBox(height: 16),
-                    _prediction.isNotEmpty
-                        ? Column(
-                      children: _prediction.map((prediction) {
-                        return Text(
-                          "Prediction: $prediction",
-                          style: TextStyle(fontSize: 16),
-                        );
-                      }).toList(),
-                    )
-                        : Container(),
+                    if (highestProbabilityPrediction.isNotEmpty)
+                      Text(
+                        highestProbabilityPrediction,
+                        style: TextStyle(fontSize: 16),
+                      ),
                     ElevatedButton(
                       onPressed: () {
                         setState(() {
-                          _popupOpen = false; // Set popup state to false.
+                          _popupOpen = false; // 팝업 상태를 닫음.
                         });
-                        Navigator.of(context).pop(); // Close the dialog.
+                        Navigator.of(context).pop(); // 팝업 닫음.
                       },
                       child: Text("OK"),
                     ),
@@ -146,8 +167,10 @@ class _HomeScreenState extends State<HomeScreen> {
                             ResultObjectDetection? highestScorePrediction =
                             objDetect.reduce((a, b) => a!.score > b!.score ? a : b);
                             if (highestScorePrediction != null) {
+                              String englishLabel = highestScorePrediction.className ?? "";
+                              String translatedLabel = labelTranslations[englishLabel] ?? englishLabel;
                               String prediction =
-                                  "${highestScorePrediction.className} (${highestScorePrediction.score.toStringAsFixed(2)})";
+                                  "$translatedLabel (${highestScorePrediction.score.toStringAsFixed(2)})";
                               setState(() {
                                 _prediction.add(prediction);
                               });
@@ -167,53 +190,92 @@ class _HomeScreenState extends State<HomeScreen> {
       },
     ).then((_) {
       setState(() {
-        _popupOpen = false; // Close the popup after it's dismissed.
+        _popupOpen = false; // 팝업이 닫힌 후 팝업 상태를 업데이트.
+        _showImageDetails = true; // 이미지 및 라벨 표시
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    String highestProbabilityLabel = ""; // 초기화
+    double highestProbability = 0.0;
+
+    // Find the label with the highest probability in _prediction
+    for (String prediction in _prediction) {
+      double probability = double.parse(
+          prediction.substring(prediction.lastIndexOf("(") + 1, prediction.lastIndexOf(")")));
+      if (probability > highestProbability) {
+        highestProbability = probability;
+        highestProbabilityLabel = prediction;
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text("OBJECT DETECTOR APP")),
       backgroundColor: Colors.white,
-      body: Row(
+      body: Column(
         children: [
-          Expanded(
-            flex: 2,
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.white,
-              child: _image != null
-                  ? Image.file(
-                _image!,
-                fit: BoxFit.contain,
-              )
-                  : null,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.white,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  if (_prediction.isNotEmpty)
-                    Text(
-                      _prediction.last,
-                      style: TextStyle(fontSize: 16),
+          SizedBox(height: 20), // 이미지 위 간격
+          if (_showImageDetails) // 이미지 및 라벨 표시 여부 체크
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    width: double.infinity,
+                    height: 300, // 이미지 높이
+                    child: Center(
+                      child: _image != null
+                          ? Image.file(
+                        _image!,
+                        fit: BoxFit.contain,
+                      )
+                          : null,
                     ),
-                ],
+                  ),
+                ),
+                SizedBox(width: 16), // 이미지와 라벨 간 간격
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    height: 300, // 라벨 높이
+                    child: Center(
+                      child: Text(
+                        highestProbabilityLabel,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          if (_showImageDetails)
+            SizedBox(height: 16), // 이미지와 텍스트 간 간격
+          if (_showImageDetails)
+            Container(
+              width: double.infinity,
+              height: 250, // 텍스트 박스 높이
+              margin: EdgeInsets.all(16), // 테두리와의 간격 조정
+              padding: EdgeInsets.all(8), // 텍스트와 테두리 간의 간격
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+              ),
+              child: Text(
+                "분리배출 방법은 다음과 같습니다. \n"
+                    "\t 1. 시계의 배터리를 제거하고, 배터리는 전지류로 분리하여 배출합니다. \n"
+                    "\t 2. 시계의 표면이나 뒷면에 재활용표시가 있는 경우, 해당 표시에 따라 플라스틱류나 금속류로 분리하여 배출합니다. \n"
+                    "\t 3. 시계의 표면이나 뒷면에 재활용표시가 없는 경우, 소형가전제품으로 분리하여 배출합니다.", // 여기에 분리 배출 방법 텍스트 추가
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
         ],
       ),
+
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           runObjectDetection().then((_) {
@@ -221,8 +283,11 @@ class _HomeScreenState extends State<HomeScreen> {
               ResultObjectDetection? highestScorePrediction =
               objDetect.reduce((a, b) => a!.score > b!.score ? a : b);
               if (highestScorePrediction != null) {
+                String englishLabel = highestScorePrediction.className ?? "";
+                String translatedLabel =
+                    labelTranslations[englishLabel] ?? englishLabel;
                 String prediction =
-                    "${highestScorePrediction.className} (${highestScorePrediction.score.toStringAsFixed(2)})";
+                    "$translatedLabel (${highestScorePrediction.score.toStringAsFixed(2)})";
                 setState(() {
                   _prediction.add(prediction);
                 });
